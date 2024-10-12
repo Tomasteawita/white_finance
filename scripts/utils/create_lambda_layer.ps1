a62cf7d89fd8ede2186c4c7a788384f3de49d5a9
Cueva <A447864@santandertecnologia.com.ar>
2024-10-12 17:15:15 -0300

    add structure portfolio to aws lambda

---
+++ scripts/utils/create_lambda_layer.ps1
@@ -0,0 +1,4 @@
+pip install -t python -r requirements.txt
+
+# cambiar la palabra libraries por un nombre descriptivo
+Compress-Archive -Path "./python" -DestinationPath "./aws_libraries.zip" -CompressionLevel Optimal
