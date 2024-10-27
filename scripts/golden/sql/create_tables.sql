ffd7dcf312e82652b2d3c795bd34d8af2fc0dc2a
Cueva <A447864@santandertecnologia.com.ar>
2024-10-27 16:21:53 -0300

    Add ETL Logic Documentation

--- scripts/golden/sql/create_tables.sql
+++ scripts/golden/sql/create_tables.sql
@@ -11,13 +11,41 @@ Con los siguientes campos:
 - dividend_yield FLOAT
 - par_value FLOAT
 */
-
 CREATE TABLE golden.securities (
-    id_securitie INT PRIMARY KEY,
+    id_securitie SERIAL PRIMARY KEY,
     ticket VARCHAR(10),
     full_name VARCHAR(100),
     securitie_type VARCHAR(50),
     financial_instrument_type VARCHAR(50),
     dividend_yield FLOAT,
     par_value FLOAT
+);
+
+-- creo o reemplazo la tabla golden.portfolios
+CREATE TABLE golden.portfolios (
+    id_portfolio SERIAL PRIMARY KEY,
+    num_month_start SMALLINT,
+    num_month_end SMALLINT,
+    name_month_start VARCHAR(10),
+    name_month_end VARCHAR(10),
+    age_start INT,
+    age_end INT,
+    total_investment FLOAT,
+);
+
+-- creo o reemplazo la tabla stocks
+CREATE TABLE golden.stocks (
+    id_stock SERIAL PRIMARY KEY,
+    id_portfolio INT,
+    id_securitie INT,
+    average_purchase_price FLOAT,
+    quantity FLOAT,
+    last_price FLOAT,
+    profit_loss_percent FLOAT,
+    dividend_yield FLOAT,
+    interest_paid FLOAT,
+    amortization_paid FLOAT,
+    partition_date VARCHAR(10),
+    FOREIGN KEY (id_securitie) REFERENCES golden.securities(id_securitie),
+    FOREIGN KEY (id_portfolio) REFERENCES golden.portfolios(id_portfolio)
 );
---
+++ scripts/golden/sql/selects.sql
@@ -0,0 +1,1 @@
+select * from golden.securities;
