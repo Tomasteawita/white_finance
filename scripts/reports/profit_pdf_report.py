463ddeda25abccf0693098dd5e44393da1de3caf
Cueva <A447864@santandertecnologia.com.ar>
2024-11-23 13:49:21 -0300

    feature reportes semanales de ganancias por activos

--- scripts/reports/profit_pdf_report.py
+++ scripts/reports/profit_pdf_report.py
@@ -49,49 +49,58 @@ def calculate_profits(df_securities):
     return df_securities
 
 def gen_report(df_securities, broker_name):
-    
+
     profit_dict = {
         'name': broker_name,
         'first_partition_date': df_securities['partition_date'].iloc[0],
         'last_partition_date': df_securities['partition_date'].iloc[-1]
     }
 
-    df_securities_first_day = df_securities[df_securities['partition_date'] == profit_dict['first_partition_date']]
-    df_securities_last_day = df_securities[df_securities['partition_date'] == profit_dict['last_partition_date']]
+    df_securities_first_day = df_securities[
+        df_securities['partition_date'] == profit_dict['first_partition_date']
+    ]
+    df_securities_last_day = df_securities[
+        df_securities['partition_date'] == profit_dict['last_partition_date']
+    ]
 
     df_securities_first_day = calculate_profits(df_securities_first_day)
     df_securities_last_day = calculate_profits(df_securities_last_day)
+
     df_securities_merged = pd.merge(df_securities_first_day, df_securities_last_day, on='ticket', suffixes=('_first', '_last'))
     df_securities_merged['ars_profit_diff'] = df_securities_merged['ars_profit_last'] - df_securities_merged['ars_profit_first']
+
     df_report = df_securities_merged[['ticket', 'ars_profit_first', 'ars_profit_last', 'ars_profit_diff']]
+
     profit_dict['df_report'] = df_report
     profit_dict['best_securitie'] = df_report["ticket"].iloc[df_report["ars_profit_diff"].idxmax()]
-    profit_dict['best_securitie_profit'] = df_report["ars_profit_diff"].max()
+    profit_dict['best_securitie_profit'] = round(df_report["ars_profit_diff"].max(),2)
     profit_dict['worst_securitie'] = df_report["ticket"].iloc[df_report["ars_profit_diff"].idxmin()]
-    profit_dict['worst_securitie_profit'] = df_report["ars_profit_diff"].min()
-    df_securities_merged['percent_profit'] = (df_securities_merged['last_price_last'] - df_securities_merged['last_price_first']) / df_securities_merged['last_price_first'] * 100
+    profit_dict['worst_securitie_profit'] = round(df_report["ars_profit_diff"].min(),2)
+
+    df_securities_merged['percent_profit'] = \
+        round((df_securities_merged['last_price_last'] - df_securities_merged['last_price_first']) / df_securities_merged['last_price_first'] * 100,2)
     df_report_percents = df_securities_merged[['ticket', 'percent_profit']]
     profit_dict['best_securitie_percent_profit'] = df_report_percents["ticket"].iloc[df_report_percents["percent_profit"].idxmax()]
     profit_dict['best_securitie_percent_profit_value'] = df_report_percents["percent_profit"].max()
     profit_dict['worst_securitie_percent_profit'] = df_report_percents["ticket"].iloc[df_report_percents["percent_profit"].idxmin()]
     profit_dict['worst_securitie_percent_profit_value'] = df_report_percents["percent_profit"].min()
 
-    profit_dict['ars_profit_first'] = df_report['ars_profit_first'].sum()
+    profit_dict['ars_profit_first'] = round(df_report['ars_profit_first'].sum(),2)
 
-    profit_dict['ars_profit_last'] = df_report['ars_profit_last'].sum()
+    profit_dict['ars_profit_last'] = round(df_report['ars_profit_last'].sum(),2)
 
-    profit_dict['ars_profit_diff'] = profit_dict['ars_profit_last'] - profit_dict['ars_profit_first']
+    profit_dict['ars_profit_diff'] = round(profit_dict['ars_profit_last'] - profit_dict['ars_profit_first'],2)
 
-    profit_dict['percentage_profit_diff'] = ((profit_dict['ars_profit_last'] - profit_dict['ars_profit_first']) / profit_dict['ars_profit_first']) * 100
+    profit_dict['percentage_profit_diff'] = round(((profit_dict['ars_profit_last'] - profit_dict['ars_profit_first']) / profit_dict['ars_profit_first']) * 100,2)
 
     return profit_dict
 
 def gen_total_profit(df_reports, context):
-
-    context['first_proffit'] = df_reports['ars_profit_first'].sum()
-    context['last_proffit'] = df_reports['ars_profit_last'].sum()
-    context['proffit_difference'] = context['last_proffit'] - context['first_proffit']
-    context['proffit_difference_percentage'] = ((context['last_proffit'] - context['first_proffit']) / context['first_proffit']) * 100
+    """Función que calcula el total de ganancias y pérdidas de todas las acciones de todos los brokers"""
+    context['first_proffit'] = round(float(df_reports['ars_profit_first'].sum()),2)
+    context['last_proffit'] = round(float(df_reports['ars_profit_last'].sum()),2)
+    context['proffit_difference'] = round(context['last_proffit'],2) - round(context['first_proffit'],2)
+    context['proffit_difference_percentage'] = round(((context['last_proffit'] - context['first_proffit']) / context['first_proffit']) * 100,2)
 
     return context
 
@@ -136,7 +145,6 @@ def html_to_pdf(html_content, output_path):
 
 
 def gen_pdf_report(context, template_html_report_path, output_pdf_report_path):
-    # env = Environment(loader=FileSystemLoader(os.getcwd()))
 
     template_html_report_path = os.getcwd() + template_html_report_path.replace('./', '/')
     template_dir = os.path.dirname(template_html_report_path)
@@ -144,13 +152,12 @@ def gen_pdf_report(context, template_html_report_path, output_pdf_report_path):
 
     env = Environment(loader=FileSystemLoader(template_dir))
 
-    # template = env.get_template(template_html_report_path)
 
     template = env.get_template(template_file)
 
     for broker in context['brokers']:
         broker['df_report'] = broker['df_report'].to_html()
-    
+
     rendered_html = template.render(context)
     html_to_pdf(rendered_html, output_pdf_report_path)
 
@@ -162,7 +169,7 @@ def main(**kwargs):
         partition_date_report = pd.Timestamp.now().strftime('%Y-%m-%d')
     else:
         partition_date_report = kwargs.get('partition_date_report')
-    
+
     print(f'Reporte de rentabilidad de la semana {partition_date_report}')
 
     context = {
@@ -173,7 +180,7 @@ def main(**kwargs):
         'proffit_difference' : None,
         'proffit_difference_percentage' : None
     }
-    
+
     user = os.environ['user']
     password = os.environ['password']
     host = os.environ['host']
@@ -184,9 +191,16 @@ def main(**kwargs):
 
     engine = sqlalchemy.create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
 
-    bullma_query_profit_per_securitie = set_queries(workings_days_week, 'Bull Market', kwargs.get('partition_date_last'), kwargs.get('partition_date_first'))
-    iol_query_profit_per_securitie = set_queries(workings_days_week, 'Invertir Online', kwargs.get('partition_date_last'), kwargs.get('partition_date_first'))
-    
+    bullma_query_profit_per_securitie = set_queries(
+        workings_days_week, 'Bull Market',
+        kwargs.get('partition_date_last'),
+        kwargs.get('partition_date_first'))
+
+    iol_query_profit_per_securitie = set_queries(
+        workings_days_week, 'Invertir Online',
+        kwargs.get('partition_date_last'), kwargs.get('partition_date_first')
+    )
+
     with engine.connect() as conn:
         df_securities_bullma = pd.read_sql(bullma_query_profit_per_securitie, conn)
         df_securities_iol = pd.read_sql(iol_query_profit_per_securitie, conn)
@@ -200,7 +214,11 @@ def main(**kwargs):
 
     context = gen_total_profit(df_reports, context)
 
-    gen_pdf_report(context, kwargs.get('template_html_report_path'), kwargs.get('output_pdf_report_path'))
+    gen_pdf_report(
+        context,
+        kwargs.get('template_html_report_path'),
+        kwargs.get('output_pdf_report_path')
+    )
 
 
 if __name__ == "__main__":
