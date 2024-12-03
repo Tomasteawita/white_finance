7522805a3e9155b721ea45166b3db5b4637b7d91
Cueva <A447864@santandertecnologia.com.ar>
2024-12-03 07:21:18 -0300

    Feature inversiones en dolares

---
+++ scripts/golden/sql/create_table_currency.sql
@@ -0,0 +1,8 @@
+
+
+-- creo una tabla en el esquema golden con dos columnas. ars_usd y date
+
+CREATE TABLE golden.currency (
+    ars_usd FLOAT,
+    date DATE
+);
--- scripts/silver/structure_bullma_portfolio.py
+++ scripts/silver/structure_bullma_portfolio.py
@@ -1,6 +1,28 @@
 """Structure portfolio html file from Bull Market broker"""
 from pandas import DataFrame
 
+
+def set_currency(row):
+    columns_ars = ['Ultimo Precio', 'PPC', 'Total']
+
+    for column in columns_ars:
+        if 'USD' in row[column].lower():
+            return 'USD'
+    
+    return 'ARS'
+
+def clean_currency(row):
+    columns_ars = ['Ultimo Precio', 'PPC', 'Total']
+
+    for column in columns_ars:
+        row[column] = row[column].replace('ARS', '')
+        row[column] = row[column].replace('USD', '')
+        row[column] = row[column].replace('.', '')
+        row[column] = row[column].replace(',', '.')
+        row[column] = float(row[column])
+
+    return row
+
 def structure_portfolio(html_data):
     """
     Structure the raw portfolio data and save it in a csv file
@@ -24,18 +46,8 @@ def structure_portfolio(html_data):
     df_portfolio['Cantidad'] = df_portfolio['Cantidad'].astype(int)
     df_portfolio['Cantidad'] = df_portfolio['Cantidad'] / 100
 
-    columns_ars = ['Ultimo Precio', 'PPC', 'Total']
-
-    for column in columns_ars:
-        df_portfolio[column] = df_portfolio[column].str.replace(
-                                    'ARS', ''
-                                    ).str.replace(
-                                    '.', ''
-                                    ).str.replace(
-                                        '.', ''
-                                    ).str.replace(
-                                        ',', '.'
-                                    ).astype(float)
+    df_portfolio['Currency'] = df_portfolio.apply(set_currency, axis=1)
+    df_portfolio = df_portfolio.apply(clean_currency, axis=1)
 
     df_portfolio['Producto'] = df_portfolio['Producto'].str.split('*')
     df_portfolio['Ticket'] = df_portfolio['Producto'].apply(lambda x: x[0]).str.strip()
